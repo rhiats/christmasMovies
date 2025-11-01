f6a2bf6285c47b624f61f14043bb6f03963a5a4d
Rhia Singh <rhiasingh@Rhias-MacBook-Pro.local>
2025-11-01 19:56:34 -0400

    New file with libraries

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
+
