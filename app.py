4379c1ef9b3d6cfc371129f159d55412e9ab648e
Rhia Singh <rhiasingh@Rhias-MacBook-Pro.local>
2025-11-01 20:02:56 -0400

    Fixed bug genres to genre

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 
+# --- Load data ---
+@st.cache_data
+def load_data():
+    df = pd.read_csv("christmas_movies.csv")
+    df['genres'] = df['genre'].str.split(', ')
+    df['description'] = df['description'].fillna('')
+    return df
+
+df = load_data()
+
+st.title("🎄 Christmas Movie Explorer")
